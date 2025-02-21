0eb6ddaa64ca2753b971ef09155ee92149b322b3
Clairemgt <claire.maugueret@gmail.com>
2025-02-21 14:23:23 +0100

    Personalization v1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let countTotal = 0
 
 function increment() {
     count += 1
@@ -8,8 +10,10 @@ function increment() {
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+    let countSaved = count
+    saveEl.textContent += countSaved + " - "
+    countTotal += countSaved
+    totalEl.textContent = countTotal
     countEl.textContent = 0
     count = 0
 }
