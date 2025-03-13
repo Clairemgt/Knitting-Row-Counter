d36f055316211551e86899c0966bca0d1239fa81
Clairemgt <claire.maugueret@gmail.com>
2025-03-13 15:10:10 +0100

    JavaScript and HTML upgrade

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let totalEl = document.getElementById("total-el")
-let count = 0
-let countTotal = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let totalEl = document.getElementById("total-el");
+let count = 0;
+let countTotal = 0;
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
+const incrementBtn = document.getElementById("increment-btn")
+incrementBtn.addEventListener("click", function() {
+    count += 1;
+    countEl.textContent = count;
+});
 
-function save() {
-    let countSaved = count
-    saveEl.textContent += countSaved + " - "
-    countTotal += countSaved
-    totalEl.textContent = countTotal
-    countEl.textContent = 0
-    count = 0
-}
+const saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", function() {
+    let countSaved = count;
+    saveEl.textContent += countSaved + " - ";
+    countTotal += countSaved;
+    totalEl.textContent = countTotal;
+    countEl.textContent = 0;
+    count = 0;
+});
